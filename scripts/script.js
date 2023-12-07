56871a014dfd391861f76863fbb607d43bbf99b4
MvAlen0304 <120381989+MvAlen0304@users.noreply.github.com>
2023-12-07 11:00:15 +0100

    Versie 7

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
 console.log("hi");
+
+var button = document.querySelector("button");
+
+button.onclick = toggleMenu;
+
+function toggleMenu() {
+	button.classList.toggle("open");
+}
