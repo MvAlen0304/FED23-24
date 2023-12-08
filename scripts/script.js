28b9e751abb0e583df6bd976b980a51d3a17c88e
MvAlen0304 <120381989+MvAlen0304@users.noreply.github.com>
2023-12-08 19:10:37 +0100

    versie 7

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,11 @@
 console.log("hi");
 
 var button = document.querySelector("button");
+var deLijst = document.querySelector("nav ul");
 
 button.onclick = toggleMenu;
 
 function toggleMenu() {
 	button.classList.toggle("open");
+	deLijst.classList.toggle("open");
 }
